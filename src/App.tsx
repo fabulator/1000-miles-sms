99961874dfea70e6e76f8812c72c70e14d7eca8d
Michal Ozogán <michal.ozogan@ceskatelevize.cz>
2024-07-18 19:21:29 +0200

    Move translations to Czech to separete file

--- src/App.tsx
+++ src/App.tsx
@@ -8,39 +8,8 @@ import {
 } from '@chakra-ui/react';
 import React, { useEffect } from 'react';
 import { FormattedMessage, IntlProvider } from 'react-intl';
+import { csMessages } from './csMessages';
 import { Form } from './Form';
-
-const messages = {
-  'bib.label': 'Startovní číslo',
-  'bib.max': 'Startovní číslo může mít maximálně 4 číslice.',
-  'bib.min': 'Startovní číslo musí být větší než nula.',
-  'bib.required': 'Vyplňte startovní číslo.',
-  by: 'Naprogramoval <link>Michal Ozogán</link>.',
-  description:
-    'Každý den mezi 18:00 a 20:00 musíte odeslat povinnou SMS. Tato aplikace neposílá SMS, ale vygeneruje zprávu podle formátu, který je nutné dodržet.',
-  'equipment.label': 'Stav vybavení',
-  'equipment.required': 'Vyplňte stav vašeho vybavení.',
-  'invalid.pattern':
-    'Neplatný formát polohy. Měl by být ve formátu X99 99.999.',
-  'latitude.label': 'Zeměpisná šířka',
-  'latitude.required': 'Vyplňte zeměpisnou šířku.',
-  'longitude.label': 'Zeměpisná délka',
-  'longitude.required': 'Vyplňte zeměpisnou délku.',
-  'mental.label': 'Psychický stav',
-  'mental.required': 'Vyplňte psychický stav.',
-  'message.label': 'Zpráva',
-  'message.required': 'Vyplňte zprávu.',
-  'not-supported':
-    'Váš prohlížeč nepodporuje geolokaci. Polohu musíte vepsat ručně.',
-  'physical.label': 'Fyzický stav',
-  'physical.required': 'Vyplňte fyzický stav.',
-  'position.error': 'Při získávání polohy došlo k chybě {error}',
-  'position.last': 'Poslední aktualizace polohy: {from}',
-  reload: 'Znovu načíst polohu',
-  status: 'Čím větší číslo, tím lepší stav',
-  submit: 'Otevřít SMS aplikaci',
-  title: 'Povinné SMS',
-};
 
 interface Props {
   locale?: 'cs' | 'en';
@@ -56,7 +25,7 @@ export const App = ({ locale = 'en' }: Props) => {
       <IntlProvider
         defaultLocale={locale}
         locale={locale}
-        messages={locale === 'cs' ? messages : undefined}
+        messages={locale === 'cs' ? csMessages : undefined}
       >
         <VStack align={'stretch'} spacing={4}>
           <Heading size={'2xl'}>
